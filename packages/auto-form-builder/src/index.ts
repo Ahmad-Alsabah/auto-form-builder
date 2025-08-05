cb442edc2e82665295d6ef331e71cfb8adf4a533
Ahmad Alsabah <ahmad.alsaba03@gmail.com>
2025-08-05 12:31:53 +0300

    initialize project structure with TypeScript configuration and Vite setup

---
+++ packages/auto-form-builder/src/index.ts
@@ -0,0 +1,7 @@
+import AutoForm from "./components/AutoForm.vue";
+import { parseSchema, type FieldSchema } from "./utils/schemaParser";
+
+export { AutoForm, parseSchema };
+export type { FieldSchema };
+
+export default AutoForm;
